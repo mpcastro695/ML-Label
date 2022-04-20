247b806348a2e00a98ed4582b868a5b28d1e45de
Martin Castro <martincastro521@gmail.com>
2022-04-19 20:41:24 -0700

    MLImageSet and MLClassSet are now MLSet JSONObject for annotation encoding AnnotationView for reusability

--- ML Label/View/Navigation/ImageDetail.swift
+++ ML Label/View/Navigation/ImageDetail.swift
@@ -11,91 +11,44 @@ struct ImageDetail: View {
     
     
     @ObservedObject var image: MLImage
-    @EnvironmentObject var classStore: MLClassSet
     
     // Used for calculating bounding box from drag gesture
     @Binding var selectedClassLabel: MLClass
-    @State private var cgSize = CGSize()
     
-    // Used for drawing a preview bound box during gesture
-    @State var showBoxPreview = false
-    @State var boxPreview = CGRect()
-    
-    @State var scale: CGFloat = 0.96
-    
-    // Labeler class contains functions for creating annotations
-    let labeler = Labeler()
+   
     
     var body: some View {
         
-        Image(nsImage: NSImage(contentsOf: image.filePath)!)
-                .resizable()
-                .scaledToFit()
-                
-                // Reports current size via SizeReader class
-                .sizeReader(size: $cgSize)
-                
-                // MARK: - Drag Gesture
-                
-                // Drag gesture used to to create bounding box. Disabled if class selection is still default option.
-                .gesture(DragGesture(minimumDistance: 5, coordinateSpace: .local)
-                            
-                            // Sets the preview box during drag gesture
-                            .onChanged({ gesture in
-                                boxPreview = CGRect(
-                                    x: gesture.startLocation.x,
-                                    y: gesture.startLocation.y,
-                                    width: gesture.location.x - gesture.startLocation.x,
-                                    height: gesture.location.y - gesture.startLocation.y)
-                                
-                                showBoxPreview = true
-                            })
-                            
-                            // Creates annotation and disables the preview box on gesture end
-                            .onEnded({ gesture in
-                                showBoxPreview = false
-                                labeler.addBox(from: gesture,
-                                                      at: cgSize,
-                                               with: selectedClassLabel.label,
-                                                      on: image)
-//                                image.createAnnotation(from: gesture, in: cgSize, with: selectedClassLabel)
-                            })
-                ).disabled(selectedClassLabel.label == "No Class Labels")
-                
-                // MARK: - Overlay
-                
-                // Adds overlay displaying bounding boxes
-                .overlay(
-                    BoxPreviews(image: image, selectedClassLabel: $selectedClassLabel, showDrawingPreview: $showBoxPreview, boxPreview: $boxPreview, labeler: labeler)
-                )
-                .scaleEffect(scale)
-                .shadow(radius: 10)
-
+        if NSImage(contentsOf: image.filePath) != nil {
             
-//MARK: - Toolbar Items
-            
-            .toolbar{
-                
-                LabelPicker(selectedClassLabel: $selectedClassLabel)
-                    .padding(.horizontal, 10)
-                
-                
-                Button(action: {image.annotations.removeLast()}, label: {
-                    Image(systemName: "arrow.uturn.backward").font(.body.weight(.heavy))
-                }).disabled(image.annotations.count == 0)
-                
-                Button(action: {print("Manually add bounding box via window")}, label: {
-                    Image(systemName: "plus.rectangle").font(.body.weight(.heavy))
-                }).disabled(selectedClassLabel.label == "No Class Labels")
-                
-                Spacer()
-                
-                Button(action: {print("Delete")}, label: {
-                    Image(systemName: "trash").font(.body.weight(.heavy))
-                })
-                
-                
+            AnnotationView(image: image, selectedClassLabel: $selectedClassLabel)
+                        
+                .toolbar{
+                            
+                    LabelPicker(selectedClassLabel: $selectedClassLabel)
+                        .padding(.horizontal, 10)
+                            
+                    Button(action: {image.annotations.removeLast()}, label: {
+                        Image(systemName: "arrow.uturn.backward").font(.body.weight(.heavy))
+                    }).disabled(image.annotations.count == 0)
+                    
+                    Button(action: {print("Manually add bounding box via window")}, label: {
+                        Image(systemName: "plus.rectangle").font(.body.weight(.heavy))
+                    }).disabled(selectedClassLabel.label == "No Class Labels")
+                    
+                    Spacer()
+                    
+                    Button(action: {print("Delete")}, label: {
+                        Image(systemName: "trash").font(.body.weight(.heavy))
+                    })
+                }
+        }else{
+            VStack{
+                Image(systemName: "photo")
+                Text("Cant find the image file")
             }
+        }
+
         
         
     }
